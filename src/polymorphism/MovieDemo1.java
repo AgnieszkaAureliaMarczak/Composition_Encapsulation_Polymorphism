87780d1ee0924ad99f419ba7a6bb375f7ae4994e
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2023-12-06 14:07:37 +0100

    Run time type testing

---
+++ src/polymorphism/MovieDemo1.java
@@ -0,0 +1,32 @@
+package polymorphism;
+
+public class MovieDemo1 {
+    public static void main(String[] args) {
+        Movie movie = Movie.getMovie("A", "Jaws");
+        movie.watchMovie();
+
+        Adventure jaws = (Adventure) Movie.getMovie("A", "Jaws");
+        jaws.watchMovie();
+
+        Object comedy = Movie.getMovie("C", "Airplane");
+        Comedy comedyMovie = (Comedy) comedy;
+        comedyMovie.watchComedy();
+
+        var airplane = Movie.getMovie("C", "Airplane");
+        airplane.watchMovie();
+
+        var plane = new Comedy("Airplane");
+        plane.watchComedy();
+
+        //a way to test an instance of an object at runtime(this way should be avoided)
+        Object unknownObject = Movie.getMovie("C", "Airplane");
+        if (unknownObject.getClass().getSimpleName() == "Comedy"){
+            Comedy c = (Comedy) unknownObject;
+            c.watchComedy();
+        } else if (unknownObject instanceof Adventure) {
+            ((Adventure) unknownObject).watchAdventure();
+        } else if (unknownObject instanceof ScienceFiction syfy) {
+            syfy.watchScienceFiction();
+        }
+    }
+}
